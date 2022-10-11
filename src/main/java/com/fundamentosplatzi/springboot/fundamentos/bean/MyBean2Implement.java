2e81824308fb6d0ec67f74b836703cc2cbff7002
DToroDev <59370848+dtoro-dev@users.noreply.github.com>
2022-10-10 23:36:19 -0300

    inyectando mis primeras dependencias

---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/bean/MyBean2Implement.java
@@ -0,0 +1,9 @@
+package com.fundamentosplatzi.springboot.fundamentos.bean;
+
+public class MyBean2Implement implements MyBean{
+
+    @Override
+    public void print() {
+        System.out.println("Hola desde mi implementacion propia de bean 2");
+    }
+}
